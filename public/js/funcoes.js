f8960709655572cff12f73e7e823a0eb6a766f7f
Jorge <jorge.lima@bandtec.com.br>
2020-12-03 12:11:24 -0300

    Sobre feito e dashboard finalizada + alguns outros ajustes

--- public/js/funcoes.js
+++ public/js/funcoes.js
@@ -9,10 +9,14 @@ function entrar_cadastro() {
 
 // Suporte
 function enviar() {
+    if(nome.value == "" || email.value == "" || pergunta_text.value == "") {
+        alert("Preencha todos os campos")
+    } else {
     alert("Sua dúvida foi enviada e em breve será respondida através de seu e-mail!")
     nome.value = "";
     email.value = "";
     pergunta_text.value = "";
+    }
 }
 
 // Dashboard
@@ -58,4 +62,23 @@ function validar_sessao() {
 
 function finalizar_sessao() {
     fetch(`/usuarios/sair/${login_usuario}`, {cache:'no-store'}); 
+}
+
+function funciona() {
+    alert("Para entender como funciona, clique nos titulos de cada bloco que será explicado cada um!")
+}
+
+function video() {
+    alert("Aqui, você verá tutoriais/dicas por vídeos de youtube recomendado com base no seu pacote! Outra leva será apresentada assim que todos os videos forem vistos.")
+}
+
+function treino() {
+    alert("Aqui, você terá uma lista diária de treinos para te manter em forma! Eles são treinos fixos diários, então tente ao máximo manter essa rotina!")
+}
+
+function assinaturas() {
+    alert("Aqui, você encontra algumas assinaturas que você pode estar fazendo para evoluir no cs!")
+    alert("Gamersclub - Aqui você irá ter acesso ao premium do site gamersclub, onde você ira poder jogar a hora que quiser além de ter acesso ao gamesacademy, onde terá aulas gravadas para uma maior evolução (beneficios descritos na assinatura!)")
+    alert("Canais do youtube - Alguns canais que você pode seguir. Tem uma lista grande de canais que possuem como conteudo principal o cs, e, que vão te ajudar bastante ou te divertir!")
+    alert("Canais da twitch- Aqui você encontra alguns canais de transmissão ao vivo que fazem diversos conteudos de cs (Ao lado dos nomes é citado a ligação do canal com o cs. Ex: Fallen, jogador profissional de csgo!")
 }
